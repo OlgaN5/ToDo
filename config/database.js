d66ea92ed167a040ee2e7bca3ff033d8d3b6845e
Olya <naumchik.olya55@gmail.com>
2023-09-11 19:10:31 +0300

    1

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
-// const {
-//     Sequelize
-// } = require('sequelize')
-// const sequelize = new Sequelize(process.env.DATABASE,process.env.DB_USER,process.env.DB_PASSWORD,{
-//     host: process.env.HOST,
-//     dialect: process.env.DIALECT
-// })
-// module.exports = sequelize
+const {
+    Sequelize
+} = require('sequelize')
+const sequelize = new Sequelize(process.env.DATABASE,process.env.DB_USER,process.env.DB_PASSWORD,{
+    host: process.env.HOST,
+    dialect: process.env.DIALECT
+})
+module.exports = sequelize
--- config/dist/database.dev.js
+++ config/dist/database.dev.js
@@ -1,9 +1,10 @@
-// const {
-//     Sequelize
-// } = require('sequelize')
-// const sequelize = new Sequelize(process.env.DATABASE,process.env.DB_USER,process.env.DB_PASSWORD,{
-//     host: process.env.HOST,
-//     dialect: process.env.DIALECT
-// })
-// module.exports = sequelize
 "use strict";
+
+var _require = require('sequelize'),
+    Sequelize = _require.Sequelize;
+
+var sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.HOST,
+  dialect: process.env.DIALECT
+});
+module.exports = sequelize;
